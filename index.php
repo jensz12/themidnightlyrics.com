d2e06b3baf5a17a519819ee1eab37ed1f16667d6
Jens Møller <jens@jens.ninja>
2025-10-07 16:45:29 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="da">
+
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>The Midnight Lyrics Bot</title>
+	<link rel="icon" href="/favicon.png">
+	<meta name="description" content="Lyrics from The Midnight songs, posted on social media.">
+	<meta name="theme-color" content="#FF0C75">
+	<meta property="og:title" content="The Midnight Lyrics Bot" />
+	<meta property="og:description" content="Lyrics from The Midnight songs, posted on social media.">
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://themidnightlyrics.com" />
+	<meta property="og:image" content="/favicon.png" />
+	<link href="https://bootswatch.com/5/quartz/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+	<script src="https://kit.fontawesome.com/1dedeee01c.js" crossorigin="anonymous"></script>
+	<script type="module" src="https://cdn.jsdelivr.net/npm/bsky-embed@latest/dist/bsky-embed.es.js" async></script>
+	<style>
+		/* Webfont: OpticianSans-Regular */
+		@font-face {
+			font-family: 'OpticianSans';
+			src: url('Optiker-K.eot');
+			/* IE9 Compat Modes */
+			src: url('Optiker-K.eot?#iefix') format('embedded-opentype'),
+				/* IE6-IE8 */
+				url('Optiker-K.woff') format('woff'),
+				/* Modern Browsers */
+				url('Optiker-K.ttf') format('truetype');
+			/* Safari, Android, iOS */
+			font-style: normal;
+			font-weight: normal;
+			text-rendering: optimizeLegibility;
+		}
+
+		body {
+			background-image: url(/back.jpg);
+			background-repeat: no-repeat;
+			background-size: cover;
+			background-position: center center;
+			background-attachment: fixed;
+			height: 100%;
+			font-family: 'OpticianSans', sans-serif;
+			padding-top: 35px;
+			padding-bottom: 35px;
+		}
+
+		.fa-instagram {
+			color: white;
+			margin: 5px;
+		}
+
+		.fa-github {
+			color: white;
+			margin: 5px;
+		}
+
+		.fa-threads {
+			color: white;
+			margin: 5px;
+		}
+
+		.fa-bluesky {
+			color: #1185FE;
+			margin: 5px;
+		}
+	</style>
+</head>
+
+<body>
+	<main role="main" id="content" class="container">
+		<article>
+			<div class="row">
+				<div class="col-lg-12 col-md-12">
+					<div class="card">
+						<div class="card-body">
+							<h1 class="display-1 text-center">The Midnight Lyrics bot</h1>
+							<div class="text-center">
+								<p>Lyrics from The Midnight songs, posted on social media</p>
+								<h2 class="display-3 text-center">Find the bot here</h2>
+							</div>
+							<h3 class="text-center">
+								<a href="https://instagram.com/the_midnight_lyrics_bot"><i class="fab fa-instagram fa-xl"></i></a>
+								<a href="https://bsky.app/profile/themidnightlyrics.com"><i class="fab fa-bluesky fa-xl"></i></a>
+								<!--<a href="https://www.threads.com/@the_midnight_lyrics_bot"><i class="fab fa-threads fa-xl"></i></a>-->
+							</h3>
+							<div class="text-center">
+								<p>Find the code for the bot and help contribute right here</p>
+							</div>
+							<h3 class="text-center">
+								<a href="https://github.com/Spirit55555/midnight-lyrics-bot/"><i class="fab fa-github"></i></a>
+							</h3>
+							<!--<hr>
+							<h2 class="display-4 text-center">Latest Bluesky post</h2>
+							  <bsky-embed username="themidnightlyrics.com" limit="2" load-more="false" disable-images="true" disable-videos="true" disable-autoplay="true"></bsky-embed>-->
+						</div>
+					</div>
+				</div>
+			</div>
+		</article>
+	</main>
+
+</html>
